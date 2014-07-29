15a8330742813d857fc6fe93325649ea175ed698
Noirbot <fortytwofortytwo@gmail.com>
2014-07-28 22:44:06 -0500

    Trying a conditional.

--- lib/git.zsh
+++ lib/git.zsh
@@ -3,8 +3,10 @@ function git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    branch=$( command git branch --contains $ref | perl -ne '/([^ ]*(?=\)))/; print "$1\n";')
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${branch}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    if [[ $ref =~ ^[0-9A-Fa-f]{7}$ ]] ; then
+      ref=$( command git branch --contains $ref | perl -ne '/([^ ]*(?=\)))/; print "$1\n";')
+    fi
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
